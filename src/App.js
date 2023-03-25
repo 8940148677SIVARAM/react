9105773b56de20f7e5dd7c48dab2c1d52eaaca1c
unknown <8940148677SIVARAM>
2023-03-25 10:19:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+function App(props) {
+   let s=props.flt;
+   
+   function siva(a){
+      let h=s.filter((s)=>a>=s.year);
+      console.log(h.map((g)=>g.name));
+   }
+  return ( 
+    <>
+      <div>
+        {s.map((s,i)=><h1 key={i}>{s.year}books<button onClick={()=>{siva(s.year)}}>click</button></h1>)}
+          
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/mobile.js
@@ -0,0 +1,36 @@
+import React, {useState, usestate} from 'react'
+import "./App.css"
+function Mobile(props){
+    let h=props.dd;
+   const [mobile,whychange]=useState(h) 
+   
+   const whysorting=(siva)=>{
+    var so=[...h].sort((a,b) => {
+        var sad=a.name.toUpperCase()
+        var sosad=b.name.toUpperCase()
+        if(sad<sosad){return -1}
+        if(sad>sosad){return 1}
+        return 0;
+ })
+     {siva=='yeru' ? whychange(so):whychange(so.reverse())}
+   }
+   let price=()=>{
+    var data=[...h].sort((a,b)=>a.price-b.price)
+    whychange(data)
+   }
+   let fil=()=>{
+    let buy=[...h].filter(s=>s.isPurchased===true)
+    whychange(buy)
+   }
+    
+    return(
+       <>
+       <button onClick={()=>whysorting("yeru")}>ascending order</button>
+       <button onClick={()=>whysorting("siva")}>decending order</button>
+       <button onClick={()=>price()}>price list</button>
+       <button onClick={()=>fil()}>isPurchased</button>
+       {mobile.map((v,i)=><h1 key={i}>{v.name}-{v.price}</h1>)}
+       </>
+    )
+}
+export default Mobile;
